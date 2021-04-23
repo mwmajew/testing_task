476fc4a903bc955c817b295625fda712d3519c8a
mwmajew <mmajewski@agh.edu.pl>
2021-04-23 11:17:00 +0200

    Create task.py

---
+++ task.py
@@ -0,0 +1,8 @@
+# Add tests using unittest here
+# at least 10 tests
+# create `your_partners_github_username.py` file and paste your partner's code there
+# if it includes other files, add the files to this repo as well
+# when you finished including your partner's code, commit it, and THEN start doing the task
+# you code should be runnable with `python -m unittest task.py`
+
+import your_partners_github_username
